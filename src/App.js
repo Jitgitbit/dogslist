e83120b5059740475e3ed4e1688c281fb3242552
Jitgitbit <rowowolf@gmail.com>
2020-01-23 14:56:55 +0100

    images link added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
 import { Route } from "react-router-dom";
-import DogBreedImages from './components/DogBreedImages';
+import DogBreedImagesContainer from './components/DogBreedImagesContainer';
 
 
 function App() {
@@ -10,10 +10,11 @@ function App() {
     <div className="App">
       <main>
       <Route exact path="/" component={DogsListContainer} />
-      <Route path="/dog-breeds/:breed" component={DogBreedImages} />
+      <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
       </main>
     </div>
   );
 }
 
 export default App;
+//
--- src/components/DogBreedImages.jsx
+++ src/components/DogBreedImages.jsx
@@ -3,12 +3,23 @@ import { Link } from "react-router-dom";
 
 
 export default function DogBreedImages (props) {
+  const { images, breed } = props
+
   return (
     <div className="dog-breed-images">
       <h1>Dogs Breed Images</h1>
-      This page will show images of a specific dog breed.
-      <Link to="/">Go back to the index</Link>
-      
+    {/* 
+    		Changed from: 
+	        This page will show images of the { props.match.params.breed } breed. to the code below.
+      	*/}
+        <div>
+        <Link to="/">Go back to the index</Link>
+        </div>
+		This page will show images of the { breed } breed
+		  <div>
+    	  { images && images.map(url => <img src={ url } alt="Dog" />) }
+  	      { !images && 'Loading...' }
+	    </div>
     </div>
   )
 }
